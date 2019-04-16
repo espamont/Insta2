36789f360505c4778907146a34cbb140f6129342
Montse <monstesparza5@gmail.com>
2019-04-16 12:38:32 -0500

    Added Insta Home file

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:insta/insta_home.dart';
+void main()=> runApp(new MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+   return new MaterialApp(
+     title: 'Insta',
+     theme: new ThemeData(
+       primaryColor: Colors.black,
+       primarySwatch: Colors.blue,
+       primaryIconTheme: IconThemeData(color: Colors.black),
+     ),
+     home: new InstaHome(),
+   );
+  }
+
+
+}
+
